b85098adb302f9d06fa7076d49ec388fc95732d1
Vinicius Liberatti Bovo <viniciusliberattibovo@gmail.com>
2026-04-15 12:53:31 -0300

    Criação de Domínio

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,7 +1,58 @@
-const User = require("../models/User")
+const User = require("../models/User");
+const bcrypt = require("bcrypt")
 
 module.exports = class UserController {
-    static async register(req, res) {
-        res.json('Olá Get Pet')
+  static async register(req, res) {
+    const { name, email, phone, password, confirmapassword } = req.body;
+
+    if (!name) {
+      res.status(422).json({ message: "Nome é obrigatório" });
+      return;
     }
-}
+    if (!email) {
+      res.status(422).json({ message: "Email é obrigatório" });
+      return;
+    }
+    if (!phone) {
+      res.status(422).json({ message: "Phone é obrigatório" });
+      return;
+    }
+    if (!password) {
+      res.status(422).json({ message: "password é obrigatório" });
+      return;
+    }
+    if (!confirmapassword) {
+      res.status(422).json({ message: "Confirmação de senha é obrigatório" });
+      return;
+    }
+    if (password !== confirmapassword) {
+      res.status(422).json({ message: "As senhas não coincidem" });
+      return;
+    }
+    const userExists = await User.findOne({ email: email });
+
+    if (userExists) {
+      res
+        .status(422)
+        .json({ message: "O Usuário já existe em nossos registros." });
+      return;
+    }
+
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const user = new User({
+      name: name,
+      email: email,
+      phone: phone,
+      password: passwordHash,
+    });
+
+    try {
+      const newUser = await user.save();
+      res.status(201).json({ message: "Usuário criado no Get Pet", newUser });
+    } catch (error) {
+      res.status(503).json({ message: error });
+    }
+  }
+};
