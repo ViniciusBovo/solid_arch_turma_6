cecdecbf7d84f4aac6d582b84cb951505a1d92a8
Vinicius Liberatti Bovo <viniciusliberattibovo@gmail.com>
2026-04-22 12:51:55 -0300

    Aula2204-Implementação JWT
    Implementação de JWT para corpo Open and Close

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,34 +1,31 @@
-const mongoose = require('../db/conn')
-const { Schema } = mongoose
+const mongoose = require("../db/conn");
+const { Schema } = mongoose;
 
 const User = mongoose.model(
-    'User',
-    new Schema({
-        name: {
-            type: String,
-            required: true
-        },
-        email: {
-            type: String,
-            required: true
-        },
-        password: {
-            type: String,
-            required: true
-        },
-        image: {
-            type: String,
-            required: true
-        },
-        phone: {
-            type: String,
-            required: true
-        },
+  "User",
+  new Schema(
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+      },
+      phone: {
+        type: String,
+        required: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
     },
-        {
-            timestamp: true
-        }
-    )
-)
+    {
+      timestamp: true,
+    }
+  )
+);
 
-module.exports = User
+module.exports = User;
