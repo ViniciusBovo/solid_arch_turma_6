527c09c088025dad1fcf401bb604eb8c27502a54
Vinicius Liberatti Bovo <viniciusliberattibovo@gmail.com>
2026-05-20 11:44:46 -0300

    feat: initialize PetController class with necessary dependencies

---
+++ backend/controllers/PetController.js
@@ -0,0 +1,9 @@
+const Pet = require('../models/Pet')
+
+const getUserByToken = require("../helpers/get-user-by-token")
+const getToken = require("../helpers/get-tokens")
+const ObjectId = require("mongoose").Types.ObjectId
+
+module.exports = class PetController{
+
+}
